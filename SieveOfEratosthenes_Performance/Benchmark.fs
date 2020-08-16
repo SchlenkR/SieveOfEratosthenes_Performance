86e5039f31a6fa450a42f8a509915e95dc0d4577
SchlenkR <ronald.schlenker@binarygears.de>
2020-08-16 19:47:31 +0200

    initial

---
+++ SieveOfEratosthenes_Performance/Benchmark.fs
@@ -0,0 +1,27 @@
+﻿namespace Benchmark
+
+open BenchmarkDotNet.Attributes
+open BenchmarkDotNet.Jobs
+
+[<SimpleJob(RuntimeMoniker.Net472, baseline = true)>]
+[<SimpleJob(RuntimeMoniker.NetCoreApp30)>]
+type SieveOfEratosthenes() =
+
+    [<Params(10_000_000, 50_000_000, 100_000_000)>]
+    let mutable n = Unchecked.defaultof<int>
+
+    [<Benchmark>]
+    member this.For() = SieveOfEratosthenes.siebFor n
+
+    [<Benchmark>]
+    member this.While() = SieveOfEratosthenes.siebWhile n
+
+    [<Benchmark>]
+    member this.Iter() = SieveOfEratosthenes.siebIter n
+
+    [<Benchmark>]
+    member this.PIter() = SieveOfEratosthenes.siebPIter n
+
+    [<Benchmark>]
+    member this.Rec() = SieveOfEratosthenes.siebRec n
+
---
+++ SieveOfEratosthenes_Performance/SieveOfEratosthenes.fs
@@ -0,0 +1,114 @@
+﻿module SieveOfEratosthenes
+
+let siebWhile n =
+    let toCheck = Array.zeroCreate<int> (n + 1)
+    let crossedV = -1
+    
+    let mutable i = 2
+    while i <= n do
+        if toCheck.[i] <> crossedV then
+            toCheck.[i] <- i
+            
+            let mutable j = i + i
+            while j <= n do
+                toCheck.[j] <- crossedV
+                j <- j + i
+        i <- i + 1
+    toCheck
+
+let siebFor n =
+    let toCheck = Array.zeroCreate<int> (n + 1)
+    let crossedV = -1
+    
+    for i in 2..n do
+        if toCheck.[i] <> crossedV then
+            toCheck.[i] <- i
+            
+            for j in (i+i).. i .. n do
+                toCheck.[j] <- crossedV
+    toCheck
+
+let siebIter n =
+    let toCheck = Array.zeroCreate<int> (n + 1)
+    let crossedV = -1
+    
+    toCheck
+    |> Array.iteri (fun i v ->
+        if i < 2 then ()
+        elif toCheck.[i] <> crossedV then
+            toCheck.[i] <- i
+            
+            for j in (i+i).. i .. n do
+                toCheck.[j] <- crossedV
+        else ())
+    toCheck
+
+let siebPIter n =
+    let toCheck = Array.zeroCreate<int> (n + 1)
+    let crossedV = -1
+    
+    toCheck |> Array.Parallel.iteri (fun i v ->
+        if i < 2 then ()
+        elif toCheck.[i] <> crossedV then
+            toCheck.[i] <- i
+
+            for j in (i+i).. i .. n do
+                toCheck.[j] <- crossedV
+        else ())
+    toCheck
+
+let siebRec n =
+    let toCheck = Array.zeroCreate<int> (n + 1)
+    let crossedV = -1
+
+    // F# has tail call optimizations and can transform that into a whie loop
+    let rec loopi i =
+        if i <= n then
+            if toCheck.[i] <> crossedV then
+                toCheck.[i] <- i
+                
+                let rec loopj j =
+                    if j <= n then
+                        toCheck.[j] <- crossedV
+                        loopj (j+i)
+                loopj (2*i)
+            loopi (i+1)
+    loopi (2)
+    
+    // return
+    toCheck
+
+
+
+module Performance =
+
+    open System.Diagnostics
+    
+    let measure name f =
+        let sw = Stopwatch()
+        sw.Start()
+        let res = f()
+        sw.Stop()
+        printfn "%50s  --  %fs" name sw.Elapsed.TotalSeconds
+        res
+    
+    let filterPrim v = v |> Array.filter (fun x -> x > 0)
+    
+    let cases = [
+        siebWhile, "while loop"
+        siebFor, "for loop"
+        siebIter, "Array.iteri (outer) for loop (inner)"
+        siebPIter, "Array.Parallel.iteri (outer) for loop (inner)"
+        siebRec, "Tail recursion (both)"
+    ]
+    
+    let benchmark() =
+        let n = 50_000_000
+
+        printfn ""
+
+        for (case, desc) in cases do
+            (fun () -> case n) |> measure desc |> ignore
+    
+        printfn ""
+        printfn ""
